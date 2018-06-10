0fb9360e6913956574acd19aa43b5eb8f4039a89
danchede <1320030382@qq.com>
2018-06-10 10:12:56 +0800

    first commit

---
+++ src/test/java/com/ifox/springbooteasypoi/SpringbooteasypoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ifox.springbooteasypoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbooteasypoiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
